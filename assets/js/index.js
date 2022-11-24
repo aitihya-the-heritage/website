f0412f6a749d2208ee66a7eb556056513455d0ce
Projckt <projckt@protonmail.com>
2022-11-24 17:42:08 +0530

    responsive and fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,17 @@
-const initialize = () => {};
+const initialize = () => {
+  const hamburgerMenu = document.getElementById("show-mobile-nav");
+  const closeMobileNavBtn = document.getElementById("close-mobile-nav");
+  const mobileNavContainer = document.getElementById("mobile-nav");
+
+  hamburgerMenu.addEventListener("click", (e) => {
+    e.preventDefault();
+    mobileNavContainer.style.display = "block";
+  });
+  closeMobileNavBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    mobileNavContainer.style.display = "none";
+  });
+};
 
 (() => {
   initialize();
